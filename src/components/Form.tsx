a115bcba06db4be8bfdd8dee4012fadbfc7857e1
hantani <17hantani@gmail.com>
2024-05-09 17:47:21 +0900

    2024-05-09 work

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -5,11 +5,36 @@ import {
   IonDatetimeButton,
   IonModal,
   IonLabel,
+  IonInput,
+  IonTextarea,
+  IonText,
+  IonButton,
 } from "@ionic/react";
-import React, { useRef } from "react";
+import React, { useState } from "react";
 
-const Form = () => {
-  const startDateRef = useRef(null);
+const Form = ({
+  startDateRef,
+  endDateRef,
+  subjectRef,
+  textAreaRef,
+  warning,
+  onAdd,
+}: {
+  startDateRef: any;
+  endDateRef: any;
+  subjectRef: any;
+  textAreaRef: any;
+  warning: any;
+  onAdd: any;
+}) => {
+  const [subject, setSubject] = useState();
+  const [textarea, setTextarea] = useState();
+  const onSubjectChange = (e: any) => {
+    setSubject(e.target.value);
+  };
+  const onTextareaChange = (e: any) => {
+    setTextarea(e.target.value);
+  };
 
   return (
     <>
@@ -25,7 +50,46 @@ const Form = () => {
             <IonDatetime id="start-date" ref={startDateRef}></IonDatetime>
           </IonModal>
         </IonItem>
+        <IonItem>
+          <IonLabel>종료 시간</IonLabel>
+          <IonDatetimeButton
+            datetime="end-date"
+            className="end-date"
+          ></IonDatetimeButton>
+          <IonModal keepContentsMounted={true}>
+            <IonDatetime id="end-date" ref={endDateRef}></IonDatetime>
+          </IonModal>
+        </IonItem>
+        <IonItem>
+          <IonInput
+            label="공부한 과목"
+            placeholder="공부한 과목"
+            onIonInput={onSubjectChange}
+            ref={subjectRef}
+          ></IonInput>
+        </IonItem>
+        <IonItem>
+          <IonTextarea
+            label="공부한 내용"
+            placeholder="공부한 내용"
+            onIonInput={onTextareaChange}
+            className="custom-text-area"
+            ref={textAreaRef}
+          ></IonTextarea>
+        </IonItem>
       </IonList>
+      {warning === true && (
+        <div className="custom-danger-wrapper">
+          <IonText color="danger">
+            시간 입력이 잘못되었습니다 다시 입력해주세요
+          </IonText>
+        </div>
+      )}
+      <div className="custom-btn-wrapper">
+        <IonButton color="primary" onClick={onAdd}>
+          추가하기
+        </IonButton>
+      </div>
     </>
   );
 };
--- src/pages/Note.tsx
+++ src/pages/Note.tsx
@@ -1,53 +1,29 @@
-import {
-  IonContent,
-  IonHeader,
-  IonInput,
-  IonItem,
-  IonList,
-  IonTitle,
-  IonToolbar,
-  IonLabel,
-  IonDatetime,
-  IonTextarea,
-  IonButton,
-  IonDatetimeButton,
-  IonModal,
-  IonText,
-} from "@ionic/react";
-import { useState, useRef, useReducer, useEffect } from "react";
+import { IonContent, IonHeader, IonTitle, IonToolbar } from "@ionic/react";
 import WiseSaying from "../components/WiseSaying";
-import { reducer, initialDateObj } from "../reducer/dateReducer";
 import { useStorage } from "../hooks/useStorage";
 import TodayStudyRecord from "../components/TodayStudyRecord";
 import TodayStudyTime from "../components/TodayStudyTime";
 import Form from "../components/Form";
+import React, { useRef, useReducer, useEffect } from "react";
+import { reducer, initialDateObj } from "../reducer/dateReducer";
 
 const Note: React.FC = () => {
-  const [subject, setSubject] = useState();
-  const [textarea, setTextarea] = useState();
+  const {
+    todayStudyTime,
+    todayStudyRecord,
+    todayGoalTime,
+    addTodayGoalTime,
+    addTodayStudyTime,
+    addTodayStudyRecord,
+    addStudyTimes,
+  } = useStorage();
+  const isMounted = useRef(false);
   const startDateRef = useRef<any>();
   const endDateRef = useRef<any>();
   const subjectRef = useRef<any>();
   const textAreaRef = useRef<any>();
   const [state, dispatch] = useReducer(reducer, initialDateObj);
-  const {
-    day,
-    todayStudyTime,
-    addTodayStudyTime,
-    todayStudyRecord,
-    addTodayStudyRecord,
-    todayGoalTime,
-    addTodayGoalTime,
-    addStudyTimes,
-  } = useStorage();
-  const isMounted = useRef(false);
 
-  const onSubjectChange = (e: any) => {
-    setSubject(e.target.value);
-  };
-  const onTextareaChange = (e: any) => {
-    setTextarea(e.target.value);
-  };
   const onAdd = () => {
     if (
       startDateRef.current.value === undefined &&
@@ -81,6 +57,7 @@ const Note: React.FC = () => {
       dispatch({ type: "BOTH_VALUE", startDateValue, endDateValue });
     }
   };
+
   useEffect(() => {
     if (isMounted.current && state.resultTime > 0) {
       const newStudyRecord = {
@@ -93,6 +70,7 @@ const Note: React.FC = () => {
       addTodayStudyTime(state.resultTime);
       addTodayStudyRecord(newStudyRecord);
       addStudyTimes(state, newStudyRecord);
+      console.log(todayStudyTime, todayStudyRecord);
     } else {
       isMounted.current = true;
     }
@@ -107,59 +85,15 @@ const Note: React.FC = () => {
       </IonHeader>
       <IonContent className="ion-padding">
         <WiseSaying></WiseSaying>
-        <Form />
+        <Form
+          startDateRef={startDateRef}
+          endDateRef={endDateRef}
+          subjectRef={subjectRef}
+          textAreaRef={textAreaRef}
+          warning={state.warning}
+          onAdd={onAdd}
+        />
         <p className="custom-heading">공부시간 기록</p>
-        <IonList>
-          <IonItem>
-            <IonLabel>시작 시간</IonLabel>
-            <IonDatetimeButton
-              datetime="start-date"
-              className="start-date"
-            ></IonDatetimeButton>
-            <IonModal keepContentsMounted={true}>
-              <IonDatetime id="start-date" ref={startDateRef}></IonDatetime>
-            </IonModal>
-          </IonItem>
-          <IonItem>
-            <IonLabel>종료 시간</IonLabel>
-            <IonDatetimeButton
-              datetime="end-date"
-              className="end-date"
-            ></IonDatetimeButton>
-            <IonModal keepContentsMounted={true}>
-              <IonDatetime id="end-date" ref={endDateRef}></IonDatetime>
-            </IonModal>
-          </IonItem>
-          <IonItem>
-            <IonInput
-              label="공부한 과목"
-              placeholder="공부한 과목"
-              onIonInput={onSubjectChange}
-              ref={subjectRef}
-            ></IonInput>
-          </IonItem>
-          <IonItem>
-            <IonTextarea
-              label="공부한 내용"
-              placeholder="공부한 내용"
-              onIonInput={onTextareaChange}
-              className="custom-text-area"
-              ref={textAreaRef}
-            ></IonTextarea>
-          </IonItem>
-        </IonList>
-        {state.warning === true && (
-          <div className="custom-danger-wrapper">
-            <IonText color="danger">
-              시간 입력이 잘못되었습니다 다시 입력해주세요
-            </IonText>
-          </div>
-        )}
-        <div className="custom-btn-wrapper">
-          <IonButton color="primary" onClick={onAdd}>
-            추가하기
-          </IonButton>
-        </div>
         <TodayStudyRecord todayStudyRecord={todayStudyRecord} />
         <TodayStudyTime
           todayStudyTime={todayStudyTime}
